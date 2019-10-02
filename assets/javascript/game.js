34b01239bf8648844661e794d30d2752526972ee
Ajay Singh <ajayh2@gmail.com>
2019-10-01 22:53:26 -0400

    added number randomly generated that is going to be goal of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,20 @@ var randomNumber;
 var wins;
 var loses;
 
+goal = Math.floor(Math.random() *99 + 19);
+
+console.log(goal);
+
+
+$("#target").html("goal:  " + goal);
+
+
 for(var i = 0; i < 4; i++){
 
     var random = Math.floor(Math.random() *12);
     console.log(random)
 
+      
     var crystal = $("<div>");
         crystal.attr({
             "class": 'crystal',
