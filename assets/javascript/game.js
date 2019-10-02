6c0ff5decba0b16f1e4e07f15e78d081e8b635f6
Ajay Singh <ajayh2@gmail.com>
2019-10-01 23:46:03 -0400

    missing incrrementing and dont have picture

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
-var randomNumber;
-var wins;
-var loses;
-
+var goal;
+var wins = 0;
+var loses = 0;
+var added = 0;
 goal = Math.floor(Math.random() *99 + 19);
 
-console.log(goal);
 
 
 $("#target").html("goal:  " + goal);
+$("#ws").html("wins:  " + wins);
+$("#ls").html("loses:  " + loses);
+
 
 
 for(var i = 0; i < 4; i++){
 
-    var random = Math.floor(Math.random() *12);
-    console.log(random)
+    var random = Math.floor(Math.random() *12 + 1);
 
       
     var crystal = $("<div>");
@@ -24,5 +25,17 @@ for(var i = 0; i < 4; i++){
 
     $(".allCrystals").append(crystal);
 
-    console.log("hello world");
 }
+$(".crystal").on("click", function (){
+    var num = parseInt($(this).attr("data-random"));
+    added += num;
+    console.log(added);
+
+    if(added > goal){
+        loses--;
+    }
+    else if(added === goal){
+        wins--;
+    }
+
+})
